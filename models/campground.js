b156c2e14cccd421cecf8d277c058785bb35d5c9
Evatitan <yinguihua2014@gmail.com>
2021-10-15 11:20:29 +0200

    creat a query middleware for campground, for delete all reviews when delete this campground

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const { campgroundSchema } = require('../schemas');
+const review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -13,6 +15,16 @@ const CampgroundSchema = new Schema({
 			ref: 'Review'
 		}
 	]
+});
+// query middleware for delete all reviews of one campground when this campground was be delete.来自mongoose
+CampgroundSchema.post('findOneAndDelete', async function(doc) {
+	if (doc) {
+		await review.deleteMany({
+			_id: {
+				$in: doc.reviews
+			}
+		});
+	}
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
