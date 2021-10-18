6ee6af899b9ad2e57d8e0941bfcddc178a03b572
Evatitan <yinguihua2014@gmail.com>
2021-10-18 19:09:28 +0200

    restructuring yelp-camp

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 //用于服务端也能准确无误的接受新创建和更改后的cambground，如果信息不完整，将会抛出错误。见post新创建， serve side validation with Joi
 const Joi = require('joi');
-const { campgroundSchema, reviewSchema } = require('./schemas');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 //用于制作虚假的put请求
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const Review = require('./models/review');
-const campground = require('./models/campground');
+
+const campgrounds = require('./routes/campgrounds');
 
 //mongoose.connect('mongoose://localhost:27017/yelp-camp', {
 //	useNewUrlParser: true,
@@ -47,7 +48,7 @@ const validateCampground = (req, res, next) => {
 	//		description: Joi.string().required()
 	//	}).required()
 	//});
-	const result = campgroundSchema.validate(req.body);
+	const { error } = campgroundSchema.validate(req.body);
 	if (error) {
 		const msg = error.detalis.map((el) => el.message).join(',');
 		throw new ExpressError(msg, 400);
@@ -73,72 +74,11 @@ app.set('views', path.join(__dirname, 'views'));
 //保证可以返回req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use('/campgrounds', campgrounds);
 
 app.get('/', (req, res) => {
 	res.render('home');
 });
-
-app.get('/', (req, res) => {
-	res.render('home');
-});
-app.get(
-	'/campgrounds',
-	catchAsync(async (req, res) => {
-		const campgrounds = await Campground.find({});
-		res.render('campgrounds/index', { campgrounds });
-	})
-);
-
-app.get('/campgrounds/new', (req, res) => {
-	res.render('campgrounds/new');
-});
-
-app.post(
-	'/campgrounds',
-	validateCampground,
-	catchAsync(async (req, res, next) => {
-		//if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
-		const campground = new Campground(req.body.campground);
-		await campground.save();
-		res.redirect(`/campgrounds/${campground._id}`);
-	})
-);
-
-app.get(
-	'/campgrounds/:id',
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		const campground = await Campground.findById(id).populate('reviews');
-		res.render('campgrounds/show', { campground });
-	})
-);
-
-app.get(
-	'/campgrounds/:id/edit',
-	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id);
-		res.render('campgrounds/edit', { campground });
-	})
-);
-
-app.put(
-	'/campgrounds/:id',
-	validateCampground,
-	catchAsync(async (req, res) => {
-		const id = req.params.id;
-		const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-		res.redirect(`/campgrounds/${campground._id}`);
-	})
-);
-
-app.delete(
-	'/campgrounds/:id',
-	catchAsync(async (req, res) => {
-		const id = req.params.id;
-		await Campground.findByIdAndDelete(id);
-		res.redirect('/campgrounds');
-	})
-);
 
 app.post(
 	'/campgrounds/:id/reviews',
