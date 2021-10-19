d7a3469591d4bdd5ebc95fbefab34babbdad202c
Evatitan <yinguihua2014@gmail.com>
2021-10-19 11:01:36 +0200

    add validateForms and stylesheet

---
+++ public/javascripts/validateForms.js
@@ -0,0 +1,19 @@
+// Bootstrap  form validation
+(function() {
+	'use strict';
+	const forms = document.querySelectorAll('.validated-form');
+	Array.from(forms).forEach(function(form) {
+		form.addEventListener(
+			'submit',
+			function(event) {
+				if (!form.checkValidity()) {
+					event.preventDefault();
+					event.stopPropagation();
+				}
+
+				form.classList.add('was-validated');
+			},
+			false
+		);
+	});
+})();
