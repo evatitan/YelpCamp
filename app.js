d19d30113380c5ac00657397312bfe62d3e712ab
Evatitan <yinguihua2014@gmail.com>
2021-10-20 12:35:38 +0200

    importe passport, passport-local,passport-local-mongoose

--- app.js
+++ app.js
@@ -7,13 +7,17 @@ const flash = require('connect-flash');
 
 //用于服务端也能准确无误的接受新创建和更改后的cambground，如果信息不完整，将会抛出错误。见post新创建， serve side validation with Joi
 const Joi = require('joi');
-
 const ExpressError = require('./utils/ExpressError');
 //用于制作虚假的put请求
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const UserRoutes = require('./routes/users');
+
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
 
 //mongoose.connect('mongoose://localhost:27017/yelp-camp', {
 //	useNewUrlParser: true,
@@ -59,6 +63,12 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
 	res.locals.success = req.flash('success');
@@ -66,8 +76,9 @@ app.use((req, res, next) => {
 	next();
 });
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', UserRoutes);
+app.use('/campgrounds', campgroundsRoutes);
+app.use('/campgrounds/:id/reviews', reviewsRoutes);
 
 app.get('/', (req, res) => {
 	res.render('home');
