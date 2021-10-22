6759ae3ca10695da21781b3fbd4ebb1a5c713fa5
Evatitan <yinguihua2014@gmail.com>
2021-10-22 18:49:36 +0200

    add custom file input for the imgs

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -54,6 +54,9 @@ module.exports.updateCampground = async (req, res) => {
 
 	//find the campground and check if the id is the same
 	const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+	const imgs = req.files.map((f) => ({ url: f.path, filename: f.filename }));
+	campground.images.push(...imgs);
+	await campground.save();
 	req.flash('success', 'Successfully updated campground!');
 	res.redirect(`/campgrounds/${campground._id}`);
 };
--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -1,6 +1,9 @@
 // Bootstrap  form validation
 (function() {
 	'use strict';
+
+	bsCustomFileInput.init();
+
 	const forms = document.querySelectorAll('.validated-form');
 	Array.from(forms).forEach(function(form) {
 		form.addEventListener(
