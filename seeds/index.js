58d3bb74f03e924e4e08160e1ba483681feb193a
Evatitan <yinguihua2014@gmail.com>
2021-10-22 17:14:52 +0200

    fix the seed in the part of the img

--- seeds/index.js
+++ seeds/index.js
@@ -30,10 +30,21 @@ const seedDB = async () => {
 			author: '616ff041a0975b1122cf639b',
 			location: `${cities[random1000].city}, ${cities[random1000].state}`,
 			title: `${sample(descriptors)} ${sample(places)}`,
-			image: 'https://source.unsplash.com/collection/483251',
 			description:
 				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos, deleniti optio! Recusandae ea veniam provident? Necessitatibus officiis quam laudantium doloribus, excepturi mollitia laborum natus repudiandae, perspiciatis, et laboriosam ducimus. Perspiciatis.',
-			price
+			price,
+			images: [
+				{
+					url:
+						'https://res.cloudinary.com/dlruca1nr/image/upload/v1634913421/YelpCamp/szfe5izlmgyo5zg1nnmy.jpg',
+					filename: 'YelpCamp/szfe5izlmgyo5zg1nnmy'
+				},
+				{
+					url:
+						'https://res.cloudinary.com/dlruca1nr/image/upload/v1634913422/YelpCamp/cysm8pgrbbwvtlnooup7.png',
+					filename: 'YelpCamp/cysm8pgrbbwvtlnooup7'
+				}
+			]
 		});
 		await camp.save();
 	}
