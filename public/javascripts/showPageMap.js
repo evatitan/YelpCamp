d638034cf5bcf3c73aa6eccb3aaeda2b307f2c28
Evatitan <yinguihua2014@gmail.com>
2021-10-26 16:16:15 +0200

    tweak style

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -6,6 +6,7 @@ const map = new mapboxgl.Map({
 	zoom: 10 // starting zoom
 });
 
+
 new mapboxgl.Marker()
 	.setLngLat(campground.geometry.coordinates)
 	.setPopup(new mapboxgl.Popup({ offset: 25 }).setHTML(`<h3>${campground.title}</h3><p>${campground.location}</p>`))
